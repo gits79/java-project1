26433d128b1dfb1f7d0ddf54e89a634a3a8c25f0
Ji Hwan <g1767g@naver.com>
2023-04-18 15:27:24 +0900

    cmit

---
+++ src/main/java/com/likelion/project1/PrintHello.java
@@ -0,0 +1,7 @@
+package com.likelion.project1;
+
+public class PrintHello {
+    public void print(){
+        System.out.println(1+1);
+    }
+}
---
+++ src/main/java/com/likelion/project1/PrintHelloTest.java
@@ -0,0 +1,9 @@
+package com.likelion.project1;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();
+        printHello.print();
+        System.out.println("j");
+    }
+}
