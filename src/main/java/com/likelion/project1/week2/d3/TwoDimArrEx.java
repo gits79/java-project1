9fb21f7db9634be7a4a8219650f040c748952b9c
Ji Hwan <g1767g@naver.com>
2023-04-26 13:05:57 +0900

    insertValue

---
+++ src/main/java/com/likelion/project1/week2/d3/TwoDimArrEx.java
@@ -0,0 +1,29 @@
+package com.likelion.project1.week2.d3;
+
+import java.util.Arrays;
+
+public class TwoDimArrEx {
+    public static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(Arrays.toString(arr[i]));
+        System.out.println();
+    }
+
+    public static void insertValue(int[][] arr, int n){
+        for(int i=0;i< arr.length;i++){
+            arr[n][i] = 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+
+        insertValue(arr,1);
+
+        printArray(arr);
+
+//        for(int i=0;i<3;i++){
+//            System.out.println(Arrays.toString(arr[i]));
+    }
+
+}
